537846b1720a99521e5b2a390a00e5da2091ac5e
Diego <diegocruzska@gmail.com>
2025-12-11 21:58:11 -0500

    Prisma fix 2

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -7,7 +7,7 @@ declare global {
 export const prisma =
   global.prisma ||
   new PrismaClient({
-    log: ["query"], // opcional
+    log: ["query"],
   });
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
